b9cd0fcc6654aa2da550c94f41663c9965f2855a
Travis Jette <travisjette@Traviss-MacBook-Pro.local>
2017-07-19 13:02:16 -0600

    learning react

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import './styles.css';
+import WebApp from './WebApp';
+
+ReactDOM.render(<WebApp/>, document.getElementById('root'));
